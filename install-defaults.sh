da4fc848ede4ce543ce9e2ff315697248768ef72
Ville Viitaharju <ville.viitaharju@festum.fi>
2019-04-24 08:58:30 +0300

    more feedback

--- install-defaults.sh
+++ install-defaults.sh
@@ -1,11 +1,15 @@
-sudo apt-get install zsh git apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+##
+echo install defaults
+sudo apt-get install zsh git apt-transport-https ca-certificates curl gnupg-agent software-properties-common python3 python3-pip
 
-# install google-chrome
+##
+echo install google-chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm google-chrome-stable_current_amd64.deb
 
-# install docker
+##
+echo install docker + docekr-compose
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
@@ -15,15 +19,16 @@ sudo add-apt-repository \
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-# check latest version at https://docs.docker.com/compose/install/
+##
+echo check latest version at https://docs.docker.com/compose/install/
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $USER
 newgrp docker
 
-# change apt mirror to FI
-sudo apt install python3 python3-pip
+##
+echo change apt mirror to FI
 sudo pip3 install apt-select
 cd /tmp
 sudo apt-select -C FI
