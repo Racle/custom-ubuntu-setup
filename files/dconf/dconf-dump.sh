96637d834d025a6cdfcc04fd0fafbe68c56188cb
Ville Viitaharju <ville.viitaharju@festum.fi>
2020-04-29 11:30:31 +0300

    removed comments

--- files/dconf/dconf-dump.sh
+++ files/dconf/dconf-dump.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.dconf
-#dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ > custom-keybindings.dconf
-#dconf dump /org/gnome/terminal/legacy/profiles:/ > terminal-profiles.dconf
-#dconf dump /com/gexperts/Tilix/ > tilix.dconf
-#dconf dump /org/gnome/shell/extensions/pop-shell/ > pop_shell.dconf
+dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.dconf
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ > custom-keybindings.dconf
+dconf dump /org/gnome/terminal/legacy/profiles:/ > terminal-profiles.dconf
+dconf dump /com/gexperts/Tilix/ > tilix.dconf
+dconf dump /org/gnome/shell/extensions/pop-shell/ > pop_shell.dconf
 
 sed -i "s/ssh .*$/ssh SSH_URL'/" custom-keybindings.dconf
