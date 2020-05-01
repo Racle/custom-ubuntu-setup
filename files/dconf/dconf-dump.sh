75841d1dc81122d255da7ee4e4b32c22c6fa95e1
Ville Viitaharju <ville.viitaharju@festum.fi>
2020-05-02 00:02:35 +0300

    updated dconf

--- files/dconf/dconf-dump.sh
+++ files/dconf/dconf-dump.sh
@@ -3,7 +3,7 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
 dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.dconf
-dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ > custom-keybindings.dconf
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > custom-keybindings.dconf
 dconf dump /org/gnome/terminal/legacy/profiles:/ > terminal-profiles.dconf
 dconf dump /com/gexperts/Tilix/ > tilix.dconf
 dconf dump /org/gnome/shell/extensions/pop-shell/ > pop_shell.dconf
--- files/dconf/dconf-load.sh
+++ files/dconf/dconf-load.sh
@@ -7,7 +7,7 @@ read ssh_url
 sed -ri "s/SSH_URL/$ssh_url/" custom-keybindings.dconf
 
 dconf load /org/gnome/desktop/wm/keybindings/ < keybindings.dconf
-dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < custom-keybindings.dconf
+dconf load /org/gnome/settings-daemon/plugins/media-keys/ < custom-keybindings.dconf
 dconf load /org/gnome/terminal/legacy/profiles:/ < terminal-profiles.dconf
 dconf load /com/gexperts/Tilix/ < tilix.dconf
 dconf load /org/gnome/shell/extensions/pop-shell/ < pop_shell.dconf
