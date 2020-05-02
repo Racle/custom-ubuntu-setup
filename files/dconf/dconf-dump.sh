4ad91535edf1701d5272a0668d9ff9a4138ae628
Ville Viitaharju <ville.viitaharju@festum.fi>
2020-05-03 00:49:42 +0300

    added preferences

--- files/dconf/dconf-dump.sh
+++ files/dconf/dconf-dump.sh
@@ -7,5 +7,6 @@ dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > custom-keybindings.d
 dconf dump /org/gnome/terminal/legacy/profiles:/ > terminal-profiles.dconf
 dconf dump /com/gexperts/Tilix/ > tilix.dconf
 dconf dump /org/gnome/shell/extensions/ > extensions.dconf
+dconf dump /org/gnome/desktop/wm/preferences/ > preferences.dconf
 
 sed -i "s/ssh .*$/ssh SSH_URL'/" custom-keybindings.dconf
--- files/dconf/dconf-load.sh
+++ files/dconf/dconf-load.sh
@@ -11,5 +11,6 @@ dconf load /org/gnome/settings-daemon/plugins/media-keys/ < custom-keybindings.d
 dconf load /org/gnome/terminal/legacy/profiles:/ < terminal-profiles.dconf
 dconf load /com/gexperts/Tilix/ < tilix.dconf
 dconf load /org/gnome/shell/extensions/ < extensions.dconf
+dconf load /org/gnome/desktop/wm/preferences/ < preferences.dconf
 
 sed -i "s/ssh .*$/ssh SSH_URL'/" custom-keybindings.dconf
