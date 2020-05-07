3e5b09f25713dbdccd80c9d278e6419fcf2b13fe
Ville Viitaharju <ville.viitaharju@festum.fi>
2020-05-07 12:55:57 +0300

    updated keybindings

--- files/dconf/dconf-load.sh
+++ files/dconf/dconf-load.sh
@@ -6,6 +6,8 @@ echo "${bold}Set SSH user@url: ${normal}"
 read ssh_url
 sed -ri "s/SSH_URL/$ssh_url/" custom-keybindings.dconf
 
+mkdir -p ~/Pictures/Screenshots
+
 dconf load /org/gnome/desktop/wm/keybindings/ < keybindings.dconf
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < custom-keybindings.dconf
 dconf load /org/gnome/terminal/legacy/profiles:/ < terminal-profiles.dconf
