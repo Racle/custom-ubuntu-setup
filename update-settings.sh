5520f6a7846cde98a23c2168bf8a1be6247f0d62
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-08-03 15:50:37 +0300

    changed to use makefile in update-settings

--- update-settings.sh
+++ update-settings.sh
@@ -13,25 +13,11 @@ echo "${bold}Setup tilix as default${normal}"
 echo ""
 sudo update-alternatives --config x-terminal-emulator
 
-
-##
-# echo ""
-# echo "${bold}Install PulseEffects${normal}"
-# echo ""
-# sudo add-apt-repository ppa:mikhailnov/pulseeffects -y
-# sudo apt-get install -y pulseeffects pulseaudio --install-recommends
-
 ##
 echo ""
 echo "${bold}Install custom dotfiles${normal}"
 echo ""
 git clone https://github.com/Racle/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
-rm ~/.zshrc ~/.p10k.zsh ~/.docker_aliases ~/.config/albert/albert.conf ~/.watchers.xml
-stow default zsh albert phpstorm
-
-sudo rm /root/.oh-my-zsh /root/.zshrc /root/.p10k.zsh /root/.docker_aliases
-sudo ln -s $HOME/.oh-my-zsh /root/
-sudo ln -s $HOME/.zshrc /root/
-sudo ln -s $HOME/.p10k.zsh /root/
-sudo ln -s $HOME/.docker_aliases /root/
+make stow
+make link-root
