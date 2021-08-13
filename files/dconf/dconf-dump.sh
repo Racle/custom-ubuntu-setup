dffbbe52d54b70ae568105aa84e496f5e7dcde31
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-08-13 14:19:41 +0300

    removed tilix configurations

--- files/dconf/dconf-dump.sh
+++ files/dconf/dconf-dump.sh
@@ -5,7 +5,6 @@ cd "$BASEDIR"
 dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.dconf
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > custom-keybindings.dconf
 dconf dump /org/gnome/terminal/legacy/profiles:/ > terminal-profiles.dconf
-dconf dump /com/gexperts/Tilix/ > tilix.dconf
 dconf dump /org/gnome/shell/extensions/ > extensions.dconf
 dconf dump /org/gnome/desktop/wm/preferences/ > preferences.dconf
 dconf dump /org/gnome/desktop/peripherals/keyboard/ > keyboard.dconf
--- files/dconf/dconf-load.sh
+++ files/dconf/dconf-load.sh
@@ -11,7 +11,6 @@ mkdir -p ~/Pictures/Screenshots
 dconf load /org/gnome/desktop/wm/keybindings/ < keybindings.dconf
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < custom-keybindings.dconf
 dconf load /org/gnome/terminal/legacy/profiles:/ < terminal-profiles.dconf
-dconf load /com/gexperts/Tilix/ < tilix.dconf
 dconf load /org/gnome/shell/extensions/ < extensions.dconf
 dconf load /org/gnome/desktop/wm/preferences/ < preferences.dconf
 dconf load /org/gnome/desktop/peripherals/keyboard/ < keyboard.dconf
