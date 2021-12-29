d8765c6f78d97dd914db7cadb9707e64d85f30d1
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-12-30 01:39:29 +0200

    added mkdir to kitty install

--- files/setup/kitty.sh
+++ files/setup/kitty.sh
@@ -3,6 +3,7 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
 # your PATH)
+mkdir -p ~/.local/bin/
 ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/
 # Place the kitty.desktop file somewhere it can be found by the OS
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
