e5b941bad3b9ed5e55a684f2a68e2912aa20f70b
Racle <759091+Racle@users.noreply.github.com>
2026-02-17 10:19:12 +0200

    updated default config (added paperwm + wayland related config)

--- files/dconf/dconf-dump.sh
+++ files/dconf/dconf-dump.sh
@@ -2,12 +2,16 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
-dconf dump /org/gnome/desktop/wm/keybindings/ > keybindings.dconf
-dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > custom-keybindings.dconf
-dconf dump /org/gnome/terminal/legacy/profiles:/ > terminal-profiles.dconf
-dconf dump /org/gnome/shell/extensions/ > extensions.dconf
-dconf dump /org/gnome/desktop/wm/preferences/ > preferences.dconf
-dconf dump /org/gnome/desktop/peripherals/keyboard/ > keyboard.dconf
-dconf dump /org/gnome/meld/ > meld.dconf
+dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.dconf
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ >custom-keybindings.dconf
+dconf dump /org/gnome/terminal/legacy/profiles:/ >terminal-profiles.dconf
+dconf dump /org/gnome/shell/extensions/ | awk '
+  /^\[/ { if ($0 ~ /^\[gsconnect|\[tiling-assistant|\[vertical-workspaces/) skip=1; else skip=0; }
+  !skip { print }
+' >extensions.dconf
+dconf dump /org/gnome/desktop/wm/preferences/ >preferences.dconf
+dconf dump /org/gnome/desktop/peripherals/keyboard/ >keyboard.dconf
+dconf dump /org/gnome/meld/ >meld.dconf
+dconf dump /org/gnome/shell/extensions/paperwm/ >paperwm.dconf
 
 sed -i "s/ssh .*$/ssh SSH_URL'/" custom-keybindings.dconf
--- files/dconf/dconf-load.sh
+++ files/dconf/dconf-load.sh
@@ -2,6 +2,14 @@
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 
+# Ask user if they want to continue with loading dconf settings
+read -p "This will overwrite your current GNOME settings. Do you want to continue? (y/n) " answer
+
+if [[ "$answer" != "y" ]]; then
+  echo "Aborting."
+  exit 0
+fi
+
 echo "${bold}Set SSH user@url: ${normal}"
 read ssh_url
 sed -ri "s/SSH_URL/$ssh_url/" custom-keybindings.dconf
@@ -14,6 +22,6 @@ dconf load /org/gnome/terminal/legacy/profiles:/ <terminal-profiles.dconf
 dconf load /org/gnome/shell/extensions/ <extensions.dconf
 dconf load /org/gnome/desktop/wm/preferences/ <preferences.dconf
 dconf load /org/gnome/desktop/peripherals/keyboard/ <keyboard.dconf
-dconf load /org/gnome/meld/ <meld.dconf
+dconf load /org/gnome/shell/extensions/paperwm/ <paperwm.dconf
 
 sed -i "s/ssh .*$/ssh SSH_URL'/" custom-keybindings.dconf
